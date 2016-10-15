33e118cb00291931b3a8cf5703c7fe1c110d0dd5
Vritrahan <sumeetbahadkar@gmail.com>
2016-10-16 01:04:27 +0530

    Login added with CSS

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    phone = models.CharField(max_length=128)
+    organization = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.user.username
--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from home import views
 
 urlpatterns = [
     url(r'^$', views.front, name='front'),
+    url(r'^register/', views.register, name='register'),
+    url(r'^login/', views.userLogin, name='login'),
 ]
