218e711bf6d48aef4385e481aab575f7141b18d1
Vritrahan <sumeetbahadkar@gmail.com>
2016-11-16 20:24:59 +0530

    Added AJAX

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,6 @@ from dashboard import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^logout/', views.userLogout, name='userLogout'),
+    url(r'^categories/', views.fetchCategory, name='fetchCategory'),
+    url(r'^items/', views.fetchItems, name='fetchItems'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import logout
+from dashboard.models import Category, Item
 import home
+import json
 
 
 def index(request):
@@ -14,3 +16,21 @@ def index(request):
 def userLogout(request):
     logout(request)
     return redirect(home.views.front)
+
+
+def fetchCategory(request):
+    responseData = {'categories': []}
+    for cat in Category.objects.all():
+        responseData['categories'].append(str(cat))
+    return HttpResponse(json.dumps(responseData), content_type="application/json")
+
+
+def fetchItems(request):
+    cat = request.GET.get('category')
+    if not cat:
+        return HttpResponse(json.dumps({}), content_type="application/json")
+    Cat = Category.objects.get(name=cat)
+    responseData = {'items': []}
+    for itm in Item.objects.get(cat=Cat):
+        responseData['items'].append(str(itm))
+    return HttpResponse(json.dumps(responseData), content_type="application/json")
